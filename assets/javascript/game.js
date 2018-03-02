f5199e150ca2d47aaa62d7dbc6722873df0f080c
Brett Rowe <brettrowe@Bretts-MacBook-Pro.local>
2018-03-02 18:35:17 -0500

    game

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+var psychicLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var youWin = 0;
+  var youLose = 0;
+  var guessesLeft = 13;
+  var guessedLetters = [];
+
+  document.onkeyup = function(event) {
+    var yourGuess = event.key; 
+    guessedLetters.push(yourGuess);    
+    var psychicAns = psychicLetters[Math.floor(Math.random() *    psychicLetters.length)];
+
+    
+    if (yourGuess === psychicAns) {
+      youWin++;
+      guessesLeft = 12;
+    }
+    
+    else {
+      guessesLeft--;
+    }
+
+    
+    if (guessesLeft === 0){
+      youLose++;
+      guessesLeft = 12;
+    }
+
+    var html = "<p><strong>Guess what letter I'm thinking of</strong>:</p>" + "<p>Wins: " + youWin + "</p>" + "<p>Loses: "+ youLose + "<p>Guesses Remaining: " + guessesLeft + "<p>Guessed Letter: " +  guessedLetters.join(', '); //Show guessed letters here
+    document.querySelector("#main").innerHTML = html;
+  }
